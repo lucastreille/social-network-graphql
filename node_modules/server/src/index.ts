3aee6ed205fe3df4f38973848bbdec3bbb2a2f27
lucastreille <lucas.treille1.0@gmail.com>
2025-02-25 17:22:15 +0100

    first commit

---
+++ node_modules/server/src/index.ts
@@ -0,0 +1,28 @@
+import { ApolloServer, gql } from 'apollo-server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    return { prisma };
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
