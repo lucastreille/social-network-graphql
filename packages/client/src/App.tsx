1b31ef14b2712ca49eff1b86f6474269fd1e3f3a
Mathis Boulais <mathis.oxym@gmail.com>
2025-02-26 16:40:18 +0100

    init codegen

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import PostDetail from './pages/PostDetail';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/post/:id" element={<PostDetail />} />
       </Routes>
     </Router>
   );
